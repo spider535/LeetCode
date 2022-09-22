e7e6e720e63003534a27f4dee78f2f8b22596254
Sagar Kesarwani <65887223+spider535@users.noreply.github.com>
2022-09-22 15:11:57 +0530

    Time: 221 ms (54.08%), Space: 106.2 MB (96.57%) - LeetHub

--- 995-minimum-number-of-k-consecutive-bit-flips/995-minimum-number-of-k-consecutive-bit-flips.cpp
+++ 995-minimum-number-of-k-consecutive-bit-flips/995-minimum-number-of-k-consecutive-bit-flips.cpp
@@ -3,21 +3,25 @@ public:
     int minKBitFlips(vector<int>& nums, int k) {
         int x = 0;
         int ans = 0;
-        queue<int> qe;
         int n = nums.size();
+        nums.push_back(0);
         for(int i = 0 ; i < n ; i++){
-            if(!qe.empty() && i == qe.front()){
+            if(nums[i] < 0){
                 x--;
-                qe.pop();
+                if(nums[i] == -1)
+                    nums[i] = 1;
+                else
+                    nums[i] = 0;
             }
             if((x%2 == 1 && nums[i] == 1) || (x%2 == 0 && nums[i] == 0)){
                 if(i+k > n)
                     return -1;
                 ans++;
-                qe.push(i+k);
+                nums[i+k] -= 2;
                 x++;
             }
         }
+        nums.pop_back();
         return ans;
     }
 };
