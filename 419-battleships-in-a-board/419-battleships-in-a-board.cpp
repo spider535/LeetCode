614fa334d7ebefab0c9b2b814fba25b6d1922695
Sagar Kesarwani <65887223+spider535@users.noreply.github.com>
2022-09-25 16:19:58 +0530

    Time: 22 ms (13.36%), Space: 8 MB (84.70%) - LeetHub

--- 419-battleships-in-a-board/419-battleships-in-a-board.cpp
+++ 419-battleships-in-a-board/419-battleships-in-a-board.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
-    void helper(vector<vector<char>>& board,int i,int j,int m,int n){
-        if(i < 0 || j < 0 || i == m || j == n || board[i][j] == '.')
-            return;
-        board[i][j] = '.';
-        helper(board,i+1,j,m,n);
-        helper(board,i-1,j,m,n);
-        helper(board,i,j+1,m,n);
-        helper(board,i,j-1,m,n);
-    }
-    int countBattleships(vector<vector<char>>& board) {
+     int countBattleships(vector<vector<char>>& board) {
         int m = board.size();
         int n = board[0].size();
         int ans = 0;
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
-                if(board[i][j] != '.'){
+                if(board[i][j] == 'X' && (i == 0 || board[i-1][j] == '.') && (j == 0 || board[i][j-1] == '.')){
                     ans++;
-                    helper(board,i,j,m,n);
                 }
             }
         }
