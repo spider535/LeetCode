b359cae4c3acada5b0afa3289187bb049a813c86
Sagar Kesarwani <65887223+spider535@users.noreply.github.com>
2022-10-01 08:59:06 +0530

    Time: 1267 ms (32.09%), Space: 173.8 MB (75.34%) - LeetHub

---
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    vector<vector<int>> ans;
+    vector<int> disc,low;
+    int timer = 0;
+    void dfs(vector<int> adj[],int u,int par){
+        disc[u] = timer;
+        low[u] = timer++;
+        for(auto &v : adj[u]){
+            if(v == par)
+                continue;
+            if(disc[v] == -1){
+                dfs(adj,v,u);
+                low[u] = min(low[u],low[v]);
+            }
+            else
+                low[u] = min(low[u],disc[v]);
+            if(low[v] > disc[u])
+                ans.push_back({u,v});
+        }
+    }
+    vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        vector<int> adj[n];
+        for(auto &it : connections){
+            adj[it[0]].push_back(it[1]);
+            adj[it[1]].push_back(it[0]);
+        }
+        disc.resize(n,-1);
+        low.resize(n,-1);
+        for(int i = 0 ; i < n ; i++){
+            if(disc[i] == -1)
+                dfs(adj,i,-1);
+        }
+        return ans;
+    }
+};
