24d33543b155d962bf8514a910e66a9b734291c4
Sagar Kesarwani <65887223+spider535@users.noreply.github.com>
2022-10-03 16:33:01 +0530

    Time: 418 ms (85.30%), Space: 115.2 MB (64.99%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.cpp
@@ -1,38 +1,32 @@
 class Solution {
 public:
-    vector<int> seg;
-    void update(long long i,int ind,int val,int tl,int tr){
-        if(tr < tl)
-            return;
-        if(tl == tr){
-            seg[i] += val;
-            return;
+    vector<int> bit;
+    int sum(int i){
+        int ans = 0;
+        while(i > 0){
+            ans+=bit[i];
+            i -= (i&-i);
         }
-        int mid = tl+(tr-tl)/2;
-        if(mid >= ind)
-            update(1ll*2*i+1,ind,val,tl,mid);
-        else
-            update(1ll*2*i+2,ind,val,mid+1,tr);
-        
-        seg[i] = seg[1ll*2*i+1]+seg[1ll*2*i+2];
+        return ans;
     }
-    int range(long long i,int l,int r,int tl,int tr){
-        if(tl > r || tr < l)
-            return 0;
-        if(tl == l && tr == r)
-            return seg[i];
-        int mid = tl+(tr-tl)/2;
-        int t1 = range(1ll*2*i+1,l,min(r,mid),tl,mid);
-        int t2 = range(1ll*2*i+2,max(mid+1,l),r,mid+1,tr);
-        return t1+t2;
+    void update(int i,int val){
+        while(i < 1e5){
+            bit[i] += val;
+            i += (i&-i);
+        }
+        
     }
     vector<int> countSmaller(vector<int>& nums) {
-        seg.resize(80000,0);
+        for(auto &it : nums){
+            it += 10000+1;
+        }
+        bit.resize(1e5,0);
         vector<int> ans;
         int n = nums.size();
-        for(int i = n-1 ; i >= 0 ; i--){
-            ans.push_back(range(0,-10000,nums[i]-1,-10000,10000));
-            update(0,nums[i],1,-10000,10000);
+        for(int i = n-1 ; i>= 0 ;i--){
+            int it = nums[i];
+            ans.push_back(sum(it-1));
+            update(it,1);
         }
         reverse(ans.begin(),ans.end());
         return ans;
