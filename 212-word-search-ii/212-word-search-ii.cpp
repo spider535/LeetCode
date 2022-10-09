8b25d3049ddf894e4f1293181f2179f876e96641
Sagar Kesarwani <65887223+spider535@users.noreply.github.com>
2022-10-09 14:46:54 +0530

    Time: 1472 ms (40.92%), Space: 16.3 MB (7.69%) - LeetHub

--- 212-word-search-ii/212-word-search-ii.cpp
+++ 212-word-search-ii/212-word-search-ii.cpp
@@ -32,35 +32,38 @@ public:
 };
 class Solution {
 public:
-    void helper(vector<vector<char>>& board,int i,int j,int m,int n,TrieNode* root,string temp,set<string>& ans){
+    void helper(vector<vector<char>>& board,int i,int j,int m,int n,TrieNode* root,string &temp,vector<string>& ans){
         if(i<0 || j<0 || i>=m || j >=n || board[i][j] == ' ')
             return;
         if(root->child[board[i][j]-'a']){
             root = root->child[board[i][j]-'a'];
-            temp += board[i][j];
-            if(root->ed)
-                ans.insert(temp);
+            temp.push_back(board[i][j]);
+            if(root->ed){
+                root->ed = false;
+                ans.push_back(temp);
+            }
             char x = board[i][j];
             board[i][j] = ' ';
             helper(board,i+1,j,m,n,root,temp,ans);
             helper(board,i,j+1,m,n,root,temp,ans);
             helper(board,i-1,j,m,n,root,temp,ans);
             helper(board,i,j-1,m,n,root,temp,ans);
+            temp.pop_back();
             board[i][j] = x;
         }
     }
     vector<string> findWords(vector<vector<char>>& board, vector<string>& words) {
         Trie* trie = new Trie(words);
         TrieNode* root = trie->getroot();
-        set<string> ans;
+        vector<string> ans;
         int m = board.size();
         int n = board[0].size();
+        string temp = "";
         for(int i = 0 ; i < m ; i++){
             for(int j = 0 ; j < n ; j++){
-                helper(board,i,j,m,n,root,"",ans);
+                helper(board,i,j,m,n,root,temp,ans);
             }
         }
-        vector<string> ans_(ans.begin(),ans.end());
-        return ans_;
+        return ans;
     }
 };
